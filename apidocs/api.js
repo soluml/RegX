1be53a70d7e0b7cb086968d24838392c3153d29d
Benjamin Solum <benjamin@soluml.com>
2013-02-06 08:28:14 -0600

    Updated docs to reflect the latest code

--- apidocs/api.js
+++ apidocs/api.js
@@ -10,7 +10,7 @@ YUI.add("yuidoc-meta", function(Y) {
         {
             "displayName": "RegX",
             "name": "RegX",
-            "description": "RegX.js\nRegX.js is a HTML5 input validation polyfill that helps you validate all W3C input types.\nKudos to Jonas Raoni Soares Silva for writing Big Number, making it easy to do to precision math with floats for the number/range inputs.\n\nCopyright 2012 Benjamin Solum (@soluml)\nReleased under the Creative Commons Attribution-ShareAlike (CC BY-SA) License.\n\nauthor:  Benjamin Solum\nversion: 0.3\nurl:     http://www.soluml.com/RegX\nsource:  https://github.com/soluml/RegX"
+            "description": "RegX.js\nRegX.js is a HTML5 input validation polyfill that helps you validate all W3C input types.\nKudos to Jonas Raoni Soares Silva for writing Big Number, making it easy to do to precision math with floats for the number/range inputs.\n\nCopyright 2013 Benjamin Solum (@soluml)\nReleased under the Creative Commons Attribution-ShareAlike (CC BY-SA) License.\n\nauthor:  Benjamin Solum\nversion: 0.4\nurl:     http://www.soluml.com/RegX/\nsource:  https://regx.github.com/"
         }
     ]
 } };
